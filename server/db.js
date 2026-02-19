c07d14fbec4e1249d45cdc7d96fb3e997a8bab6c
aaryapatil <aaryapatil1113@gmail.com>
2026-02-19 11:36:39 -0500

    server files added

---
+++ server/db.js
@@ -0,0 +1,20 @@
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const client = new MongoClient(process.env.MONGODB_URI);
+
+let db;
+
+async function connectDB() {
+    await client.connect();
+    db = client.db("clubmatch");
+    console.log("Connected to MongoDB");
+}
+
+function getDB() {
+    return db;
+}
+
+export { connectDB, getDB };
---
+++ server/server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./db.js";
+import clubsRouter from "./routes/clubs.js";
+import logsRouter from "./routes/membership-logs.js";
+import usersRouter from "./routes/users.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.static("public"));
+
+app.use("/api/clubs", clubsRouter);
+app.use("/api/membership-logs", logsRouter);
+app.use("/api/users", usersRouter);
+
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+});
